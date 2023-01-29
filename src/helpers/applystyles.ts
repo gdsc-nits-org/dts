bd9529a5f764ecc2c4a5c3d03b9aa5609f9e3e98
Jaydeep Das <jaydeepjd.8914@gmail.com>
2023-01-29 19:55:15 +0530

    BUGFIX: CSSStyleDeclarations cannot be set using index.
    Use get* and set* functions
    
    See this for reference: https://github.com/facebook/react/issues/15375

--- src/helpers/applystyles.ts
+++ src/helpers/applystyles.ts
@@ -5,7 +5,11 @@ Apply styles from one node to cloned node.
 function applystyles(node: HTMLElement, cloned: HTMLElement): void {
     const styles = window.getComputedStyle(node);
     Object.keys(styles).map((val: any) => {
-        cloned.style[val] = styles[val];
+        cloned.style.setProperty(
+            val,
+            styles.getPropertyValue(val),
+            styles.getPropertyPriority(val)
+        )
     });
 }
 
