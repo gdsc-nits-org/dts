b566c1f345dbd2e4c05504df0331d9c7216730e9
Saurav Pal <resyfer.dev@gmail.com>
2023-02-02 14:36:37 +0530

    ADD: Version bump

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ function dts(node: HTMLElement | undefined, dataURI = true) {
 	if (!node || !(node instanceof HTMLElement) || typeof dataURI !== "boolean") {
 		return "";
 	}
+
 	return tosvg(node, dataURI);
 }
 
